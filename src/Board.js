4a38883dd2a66237a67071e607daf59bddbceb71
horlarhyinka <danijufarouq2003@gmail.com>
2023-01-18 15:47:05 +0100

    implemented drag and drop with dragula library

--- src/Board.js
+++ src/Board.js
@@ -64,7 +64,6 @@ export default class Board extends React.Component {
           this.resetUi()
         })
   }
-
   getClients() {
     return [
       ['1','Stark, White and Abbott','Cloned Optimal Architecture', 'in-progress'],
@@ -99,10 +98,6 @@ export default class Board extends React.Component {
     return (
       <Swimlane name={name} clients={clients} dragulaRef={ref}/>
     );
-  }
-
-  componentWillUnmount = () =>{
-    console.log("ready to unmount")
   }
 
   render() {
