059eddc641c08dc09f60203c190686d55b4cc165
E-Zbox <zboxstudios97@gmail.com>
2023-02-25 20:06:49 +0100

    Refactored lines of code

--- src/Card.js
+++ src/Card.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
 
 export default class Card extends React.Component {
-  render() {
-    let className = ['Card'];
-    if (this.props.status === 'backlog') {
-      className.push('Card-grey');
-    } else if (this.props.status === 'in-progress') {
-      className.push('Card-blue');
-    } else if (this.props.status === 'complete') {
-      className.push('Card-green');
+    render() {
+        let className = ["Card"];
+        if (this.props.status === "backlog") {
+            className.push("Card-grey");
+        } else if (this.props.status === "in-progress") {
+            className.push("Card-blue");
+        } else if (this.props.status === "completed") {
+            className.push("Card-green");
+        }
+        return (
+            <div
+                className={className.join(" ")}
+                data-id={this.props.id}
+                data-status={this.props.status}
+            >
+                <div className="Card-title">{this.props.name}</div>
+            </div>
+        );
     }
-    return (
-      <div className={className.join(' ')} data-id={this.props.id} data-status={this.props.status}>
-        <div className="Card-title">{this.props.name}</div>
-      </div>
-    );
-  }
 }
