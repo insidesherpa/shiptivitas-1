c2a2ccfea899f31263566099fa10452fe06a48f1
E-Zbox <zboxstudios97@gmail.com>
2023-02-27 01:57:01 +0100

    added docstring comment to moveElementToIndex

--- src/utils/board.js
+++ src/utils/board.js
@@ -1,3 +1,10 @@
+/**
+ *
+ * @param {any} element item to be added into array
+ * @param {Number} index if -1 element is placed at the end of the array else, in the specified index
+ * @param {Array} array Source array
+ * @returns [] new array with element in provided index
+ */
 export const moveElementToIndex = (element, index, array) => {
     if (index === -1) {
         return [...array, element];
