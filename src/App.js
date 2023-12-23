f413e8330ad90d0d7b9ef5305210d529c20a88f6
shreyashap <107754587+shreyashap@users.noreply.github.com>
2023-12-23 14:09:35 +0530

    Task 1 Completed

--- src/App.js
+++ src/App.js
@@ -1,54 +1,49 @@
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import HomeTab from './HomeTab';
-import Navigation from './Navigation';
-import Board from './Board';
-import './App.css';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import HomeTab from "./HomeTab";
+import Navigation from "./Navigation";
+import Board from "./Board";
+import "./App.css";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedTab: 'home',
-    };
-  }
-  renderShippingRequests() {
-    return (<Board />);
-  }
+// Changed all the components into functional component!!
+// React State plays main role with the dragula Library
+function App() {
+  const [selectedTab, setSelectedTab] = useState("home");
 
-  renderNavigation() {
-    return (<Navigation
-      onClick={(tabName) => this.changeTab(tabName)}
-      selectedTab={this.state.selectedTab}
-      />);
-  }
+  const changeTab = (tabName) => {
+    setSelectedTab(tabName);
+  };
 
-  renderTabContent() {
-    switch(this.state.selectedTab) {
-      case 'home':
-      default:
-        return HomeTab();
-      case 'shipping-requests':
-        return this.renderShippingRequests();
-    }
-  }
-  render() {
+  const renderShippingRequests = () => {
+    return <Board />;
+  };
+
+  const renderNavigation = () => {
     return (
-      <div className="App">
-        {this.renderNavigation()}
-
-        <div className="App-body">
-          {this.renderTabContent()}
-        </div>
-      </div>
+      <Navigation
+        onClick={(tabName) => changeTab(tabName)}
+        selectedTab={selectedTab}
+      />
     );
-  }
+  };
 
-  changeTab(tabName) {
-    this.setState({
-      selectedTab: tabName,
-    });
-  }
+  const renderHomeTabContent = () => {
+    switch (selectedTab) {
+      case "home":
+      default:
+        return <HomeTab />;
+      case "shipping-requests":
+        return renderShippingRequests();
+    }
+  };
+
+  return (
+    <div className="App">
+      {renderNavigation()}
+
+      <div className="App-body">{renderHomeTabContent()}</div>
+    </div>
+  );
 }
 
 export default App;
--- src/HomeTab.js
+++ src/HomeTab.js
@@ -1,12 +1,10 @@
-import React from 'react';
+import React from "react";
 
 export default function HomeTab() {
   return (
     <header className="App-header">
       <div className="App-title">
-        <h1>
-          SHIPTIVITAS
-        </h1>
+        <h1>SHIPTIVITAS</h1>
         <div className="App-icon">
           <div className="Circle"></div>
         </div>
